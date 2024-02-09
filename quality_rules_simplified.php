df6e6e7528b426d8c13a31b735fee78359b0d7df
Richard Dooley <richard.dooley@nottingham.ac.uk>
2024-02-09 09:43:23 +0000

    Added difference highlighting to the data quality rules simplified view.

--- quality_rules_simplified.php
+++ quality_rules_simplified.php
@@ -21,21 +21,159 @@ while ( $infoRule = $queryRules->fetch_assoc() )
 	$listRules[] = $infoRule;
 }
 
+// Build the exportable data structure.
+$listExport = [ 'simplified_view' => 'quality_rules', 'rules' => $listRules ];
+
+// Handle upload. Get old/new data structures.
+$fileLoadError = false;
+if ( isset( $_POST['simp_view_diff_mode'] ) && $_POST['simp_view_diff_mode'] == 'export' )
+{
+	header( 'Content-Type: application/json' );
+	header( 'Content-Disposition: attachment; filename="simplified_view.json"' );
+	echo json_encode( $listExport );
+	exit;
+}
+$listOld = $listExport;
+$listNew = $listExport;
+if ( isset( $_POST['simp_view_diff_mode'] ) )
+{
+	try
+	{
+		if ( ! is_uploaded_file( $_FILES['simp_view_diff_file']['tmp_name'] ) )
+		{
+			throw new \Exception();
+		}
+		$fileData = json_decode( file_get_contents( $_FILES['simp_view_diff_file']['tmp_name'] ),
+		                         true );
+		if ( $fileData == null || ! is_array( $fileData ) ||
+		     $fileData['simplified_view'] != 'quality_rules' )
+		{
+			throw new \Exception();
+		}
+		if ( $_POST['simp_view_diff_mode'] == 'old' )
+		{
+			$listOld = $fileData;
+		}
+		elseif ( $_POST['simp_view_diff_mode'] == 'new' )
+		{
+			$listNew = $fileData;
+		}
+		else
+		{
+			throw new \Exception();
+		}
+	}
+	catch ( \Exception $e )
+	{
+		$fileLoadError = true;
+	}
+}
+
+
+// Create the combined data structure and determine whether the rules match.
+$listRules = [];
+$mapRulesN2O = [];
+foreach ( $listNew['rules'] as $i => $itemNewRule )
+{
+	foreach ( $listOld['rules'] as $j => $itemOldRule )
+	{
+		if ( $itemNewRule['rule_name'] == $itemOldRule['rule_name'] &&
+		     $itemNewRule['rule_logic'] == $itemOldRule['rule_logic'] )
+		{
+			$mapRulesN2O[ $i ] = $j;
+			continue 2;
+		}
+	}
+}
+foreach ( $listNew['rules'] as $i => $itemNewRule )
+{
+	if ( isset( $mapRulesN2O[ $i ] ) )
+	{
+		continue;
+	}
+	foreach ( $listOld['rules'] as $j => $itemOldRule )
+	{
+		if ( ! in_array( $j, $mapRulesN2O ) &&
+		     $itemNewRule['rule_logic'] == $itemOldRule['rule_logic'] )
+		{
+			$mapRulesN2O[ $i ] = $j;
+			continue 2;
+		}
+	}
+}
+foreach ( $listNew['rules'] as $i => $itemNewRule )
+{
+	if ( isset( $mapRulesN2O[ $i ] ) )
+	{
+		continue;
+	}
+	foreach ( $listOld['rules'] as $j => $itemOldRule )
+	{
+		if ( ! in_array( $j, $mapRulesN2O ) &&
+		     $itemNewRule['rule_name'] == $itemOldRule['rule_name'] )
+		{
+			$mapRulesN2O[ $i ] = $j;
+			continue 2;
+		}
+	}
+}
+
+// Add the rules to the combined data structure.
+foreach ( $listNew['rules'] as $i => $itemNewRule )
+{
+	$itemNewRule['new'] = ! isset( $mapRulesN2O[ $i ] );
+	$itemNewRule['changed_name'] = false;
+	$itemNewRule['changed_logic'] = false;
+	$itemNewRule['changed_rte'] = false;
+	$itemNewRule['deleted'] = false;
+	$itemNewRule['old_rule_id'] = '';
+	if ( ! $itemNewRule['new'] )
+	{
+		$itemOldRule = $listOld['rules'][ $mapRulesN2O[ $i ] ];
+		if ( $itemNewRule['rule_name'] != $itemOldRule['rule_name'] )
+		{
+			$itemNewRule['changed_name'] = true;
+		}
+		if ( $itemNewRule['rule_logic'] != $itemOldRule['rule_logic'] )
+		{
+			$itemNewRule['changed_logic'] = true;
+		}
+		if ( $itemNewRule['real_time_execute'] != $itemOldRule['real_time_execute'] )
+		{
+			$itemNewRule['changed_rte'] = true;
+		}
+		$itemNewRule['old_rule_id'] = $itemOldRule['rule_id'];
+	}
+	$listRules[] = $itemNewRule;
+}
+foreach ( $listOld['rules'] as $i => $itemOldRule )
+{
+	if ( ! in_array( $i, $mapRulesN2O ) )
+	{
+		$itemOldRule['new'] = false;
+		$itemOldRule['changed_name'] = false;
+		$itemOldRule['changed_logic'] = false;
+		$itemOldRule['changed_rte'] = false;
+		$itemOldRule['deleted'] = true;
+		$itemOldRule['old_rule_id'] = $itemOldRule['rule_id'];
+		$itemOldRule['rule_id'] = '';
+		$listRules[] = $itemOldRule;
+	}
+}
+
 
 // Display the project header
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
 $module->provideSimplifiedViewTabs( 'quality_rules' );
+$tblHdrStyle = REDCapUITweaker::STL_CEL . ';' . REDCapUITweaker::STL_HDR .
+               ';background:' . REDCapUITweaker::BGC_HDR;
 
 ?>
 <div class="projhdr"><i class="fas fa-clipboard-check"></i> Data Quality</div>
 <script type="text/javascript">
   $(function()
   {
-    var vStyle = $('<style type="text/css">.simpRulesTable{width:95%} .simpRulesTable th, ' +
-                   '.simpRulesTable td{border:solid 1px #000;padding:5px;vertical-align:top} ' +
-                   '.simpRulesTable th{background-color:#ddd;font-size:1.1em}</style>')
-    $('head').append(vStyle)
     var vFuncSelect = function()
     {
       var vElem = $('.simpRulesTable')[0]
@@ -59,38 +197,84 @@ $module->provideSimplifiedViewTabs( 'quality_rules' );
       }
     }
     $('#showHideID').click(vFuncShowHideID)
+    vFuncShowHideID()
   })
 </script>
-<div>
+<div style="margin-bottom:15px">
  <button class="jqbuttonmed invisible_in_print ui-button ui-corner-all ui-widget"
-         id="showHideID" style="margin-bottom:15px">Show/Hide ID</button>
+         id="showHideID">Show/Hide ID</button>
  &nbsp;
  <button class="jqbuttonmed invisible_in_print ui-button ui-corner-all ui-widget"
-         id="selectTable" style="margin-bottom:15px">Select table</button>
+         id="selectTable">Select table</button>
+ &nbsp;
+ <button class="jqbuttonmed invisible_in_print ui-button ui-corner-all ui-widget"
+         id="simplifiedViewDiffBtn">Difference highlight</button>
 </div>
-<table class="simpRulesTable">
+<table class="simpRulesTable" style="width:95%">
  <tr>
-  <th class="colID"><?php echo $GLOBALS['lang']['multilang_73'] ?? 'ID'; ?></th>
-  <th><?php echo $GLOBALS['lang']['dataqueries_15']; ?></th>
-  <th><?php echo $GLOBALS['lang']['dataqueries_16']; ?></th>
-  <th><?php echo $GLOBALS['lang']['dataqueries_123']; ?></th>
+  <th style="<?php echo $tblHdrStyle; ?>"
+      class="colID"><?php echo $GLOBALS['lang']['multilang_73'] ?? 'ID'; ?></th>
+  <th style="<?php echo $tblHdrStyle; ?>"><?php echo $GLOBALS['lang']['dataqueries_15']; ?></th>
+  <th style="<?php echo $tblHdrStyle; ?>"><?php echo $GLOBALS['lang']['dataqueries_16']; ?></th>
+  <th style="<?php echo $tblHdrStyle; ?>"><?php echo $GLOBALS['lang']['dataqueries_123']; ?></th>
  </tr>
 <?php
-foreach ( $listRules as $infoRule )
+if ( empty( $listRules ) )
 {
 ?>
  <tr>
-  <td class="colID"><?php echo intval( $infoRule['rule_id'] ); ?></td>
-  <td><?php echo $module->escapeHTML( $infoRule['rule_name'] ); ?></td>
-  <td><?php echo $module->escapeHTML( $infoRule['rule_logic'] ); ?></td>
-  <td><?php echo $lookupYN[ $infoRule['real_time_execute'] ]; ?></td>
+  <td style="<?php echo REDCapUITweaker::STL_CEL; ?>" class="colID"></td>
+  <td style="<?php echo REDCapUITweaker::STL_CEL; ?>" colspan="3">
+   0 <?php echo $GLOBALS['lang']['dataqueries_81']; ?>.
+  </td>
+ </tr>
+<?php
+}
+foreach ( $listRules as $infoRule )
+{
+	$tblIDStyle = REDCapUITweaker::STL_CEL .
+	              ( $infoRule['new'] ? ';background:' . REDCapUITweaker::BGC_NEW : '' ) .
+	              ( $infoRule['deleted'] ? ';background:' . REDCapUITweaker::BGC_DEL . ';' .
+	                                       REDCapUITweaker::STL_DEL : '' );
+	if ( $infoRule['new'] || $infoRule['deleted'] )
+	{
+		$tblNameStyle = $tblIDStyle;
+		$tblLogicStyle = $tblIDStyle;
+		$tblRTEStyle = $tblIDStyle;
+	}
+	else
+	{
+		$tblNameStyle = REDCapUITweaker::STL_CEL .
+		             ( $infoRule['changed_name'] ? ';background:' . REDCapUITweaker::BGC_CHG : '' );
+		$tblLogicStyle = REDCapUITweaker::STL_CEL .
+		            ( $infoRule['changed_logic'] ? ';background:' . REDCapUITweaker::BGC_CHG : '' );
+		$tblRTEStyle = REDCapUITweaker::STL_CEL .
+		              ( $infoRule['changed_rte'] ? ';background:' . REDCapUITweaker::BGC_CHG : '' );
+	}
+?>
+ <tr>
+  <td style="<?php echo $tblIDStyle; ?>" class="colID">
+   <?php echo ( $infoRule['rule_id'] == '' ? '' : intval( $infoRule['rule_id'] ) ), ' ',
+              ( $infoRule['old_rule_id'] == '' ||
+                $infoRule['rule_id'] == $infoRule['old_rule_id'] ? '' :
+                 ( '(' . intval( $infoRule['old_rule_id'] ) . ')' ) ), "\n"; ?>
+  </td>
+  <td style="<?php echo $tblNameStyle; ?>">
+   <?php echo $module->escapeHTML( $infoRule['rule_name'] ), "\n"; ?>
+  </td>
+  <td style="<?php echo $tblLogicStyle; ?>">
+   <?php echo $module->escapeHTML( $infoRule['rule_logic'] ), "\n"; ?>
+  </td>
+  <td style="<?php echo $tblRTEStyle; ?>">
+   <?php echo $lookupYN[ $infoRule['real_time_execute'] ], "\n"; ?>
+  </td>
  </tr>
 <?php
 }
 ?>
 </table>
 <?php
-$module->ffFormattingFix( '.simpRulesTable' );
+$module->provideSimplifiedViewDiff();
 
 // Display the project footer
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
