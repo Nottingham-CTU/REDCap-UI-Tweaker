121f35dee88b1e99370d1716e6a9286469a575a0
Richard Dooley <richard.dooley@nottingham.ac.uk>
2024-02-09 16:28:20 +0000

    Refactored code and amended layout on user rights simplified view.

--- user_rights_simplified.php
+++ user_rights_simplified.php
@@ -9,16 +9,16 @@ if ( !isset( $_GET['pid'] ) || ! $module->getSystemSetting('user-rights-simplifi
 	exit;
 }
 
-$lookupYN = [ '1' => $GLOBALS['lang']['design_100'], '0' => $GLOBALS['lang']['design_99'] ];
-$lookupDRW = [ '0' => $GLOBALS['lang']['rights_47'],
+$lookupYN = [ '1' => $GLOBALS['lang']['design_100'], '0' => '' ];
+$lookupDRW = [ '0' => '',
                '1' => $GLOBALS['lang']['dataqueries_143'],
                '2' => $GLOBALS['lang']['dataqueries_138'],
                '3' => $GLOBALS['lang']['dataqueries_139'],
                '4' => $GLOBALS['lang']['dataqueries_289'],
                '5' => $GLOBALS['lang']['dataqueries_290'] ];
-$lookupLock = [ '0' => $GLOBALS['lang']['global_23'],
-                '1' => $GLOBALS['lang']['rights_115'],
-                '2' => $GLOBALS['lang']['rights_116'] ];
+$lookupLock = [ '0' => '',
+                '1' => $GLOBALS['lang']['global_89'],
+                '2' => $GLOBALS['lang']['global_89'] . ' (' . $GLOBALS['lang']['global_34'] . ')' ];
 $lookupExport = [ '1' => $GLOBALS['lang']['rights_49'],
                   '2' => $GLOBALS['lang']['rights_48'],
                   '3' => $GLOBALS['lang']['data_export_tool_182']
@@ -94,298 +94,150 @@ $module->provideSimplifiedViewTabs( 'user_rights' );
  <button class="jqbuttonmed invisible_in_print ui-button ui-corner-all ui-widget"
          id="selectTable" style="margin-bottom:15px">Select table</button>
 </div>
+<?php
+if ( empty( $listRoles ) )
+{
+?>
 <table class="simpRolesTable">
  <tr>
-  <th><?php echo $GLOBALS['lang']['rights_198']; ?></th>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <th><?php echo $module->escapeHTML( $infoRole['role_name'] ); ?></th>
+  <td style="<?php echo REDCapUITweaker::STL_CEL; ?>">
+   0 <?php echo $GLOBALS['lang']['data_export_tool_239'], "\n"; ?>
+  </td>
+ </tr>
+</table>
 <?php
 }
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_135']; /* project design and setup */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
+else
 {
 ?>
-  <td><?php echo $lookupYN[ $infoRole['design'] ]; ?></td>
+<table class="simpRolesTable" style="width:95%">
 <?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_05']; /* user rights */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['user_rights'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['global_22']; /* data access groups */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['data_access_groups'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_08']; /* calendar */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['calendar'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_356']; /* add/edit reports */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['reports'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo htmlspecialchars( $GLOBALS['lang']['report_builder_78'] ); /* stats */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['graphical'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_01']; /* data import */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['data_import_tool'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_02']; /* data comparison */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['data_comparison_tool'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_07']; /* logging */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['data_logging'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_04']; /* file repository */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['file_repository'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_21']; /* rando */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-	echo '  <td>';
-	$list = [];
-	foreach ( [ 'random_setup' => $GLOBALS['lang']['rights_142'],
-	            'random_dashboard' => $GLOBALS['lang']['rights_143'],
-	            'random_perform' => $GLOBALS['lang']['rights_144'] ] as $key => $label )
-	{
-		if ( $infoRole[ $key ] == '1' )
-		{
-			$list[] = $label;
-		}
-	}
-	echo empty( $list ) ? $lookupYN['0'] : implode( ', ', $list );
-	echo "</td>\n";
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_20']; /* data quality */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-	echo '  <td>';
-	$list = [];
-	foreach ( [ 'data_quality_design' => $GLOBALS['lang']['design_248'] . '/' .
-	                                        $GLOBALS['lang']['global_27'],
-	            'data_quality_execute' => $GLOBALS['lang']['dataqueries_80'] ] as $key => $label )
-	{
-		if ( $infoRole[ $key ] == '1' )
-		{
-			$list[] = $label;
-		}
-	}
-	echo empty( $list ) ? $lookupYN['0'] : implode( ', ', $list );
-	echo "</td>\n";
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['dataqueries_137']; /* data resolution workflow */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupDRW[ $infoRole['data_quality_resolution'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['setup_77']; /* api */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-	echo '  <td>';
-	$list = [];
-	foreach ( [ 'api_export' => $GLOBALS['lang']['rights_139'],
-	            'api_import' => $GLOBALS['lang']['rights_314'] ] as $key => $label )
-	{
-		if ( $infoRole[ $key ] == '1' )
-		{
-			$list[] = $label;
-		}
-	}
-	echo empty( $list ) ? $lookupYN['0'] : implode( ', ', $list );
-	echo "</td>\n";
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['global_118']; /* mobile app */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['mobile_app'] ],
-                 $infoRole['mobile_app_download_data'] == '1'
-                   ? ( ' (+' . $GLOBALS['lang']['design_121'] . ')' ) : ''; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_99']; /* create records */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['record_create'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_100']; /* rename records */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['record_rename'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_101']; /* delete records */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['record_delete'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['app_11']; /* record locking customization */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['lock_record_customize'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_97'] . ' ' .
-                 $GLOBALS['lang']['rights_371']; /* lock/unlock (instrument) */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupLock[ $infoRole['lock_record'] ]; ?></td>
-<?php
-}
-?>
- </tr>
- <tr>
-  <td><?php echo $GLOBALS['lang']['rights_370']; /* lock/unlock (record) */ ?></td>
-<?php
-foreach ( $listRoles as $infoRole )
-{
-?>
-  <td><?php echo $lookupYN[ $infoRole['lock_record_multiform'] ]; ?></td>
-<?php
-}
-?>
- </tr>
-<?php
+$listModuleData = [];
 foreach ( $listModules as $moduleID => $moduleName )
 {
-?>
- <tr>
-  <td><?php echo htmlspecialchars( $moduleName ); ?></td>
-<?php
-	foreach ( $listRoles as $infoRole )
+	$listModuleData[] = [ $moduleName, $moduleID, true ];
+}
+foreach ( array_merge( [
+            // Highest Level Privileges header
+            [ ucwords( str_replace( ':', '', $GLOBALS['lang']['rights_299'] ) ), true ],
+            // - Project design and setup
+            [ $GLOBALS['lang']['rights_135'], 'design', $lookupYN ],
+            // - User rights
+            [ $GLOBALS['lang']['app_05'], 'user_rights', $lookupYN ],
+            // - Data access groups
+            [ $GLOBALS['lang']['global_22'], 'data_access_groups', $lookupYN ],
+            // Basic Privileges header
+            [ $GLOBALS['lang']['rights_198'] ?? $GLOBALS['lang']['rights_431'], true ],
+            // - Manage MyCap participants
+            [ $GLOBALS['lang']['rights_437'], 'mycap_participants', $lookupYN ],
+            // - Survey distribution tools
+            [ $GLOBALS['lang']['app_24'], 'participants', $lookupYN ],
+            // - Calendar
+            [ $GLOBALS['lang']['app_08'], 'calendar', $lookupYN ],
+            // - Add/edit reports
+            [ $GLOBALS['lang']['rights_356'], 'reports', $lookupYN ],
+            // - Stats and charts
+            [ $GLOBALS['lang']['report_builder_78'], 'graphical', $lookupYN ],
+            // - Data import
+            [ $GLOBALS['lang']['app_01'], 'data_import_tool', $lookupYN ],
+            // - Data comparison
+            [ $GLOBALS['lang']['app_02'], 'data_comparison_tool', $lookupYN ],
+            // - Logging
+            [ $GLOBALS['lang']['app_07'], 'data_logging', $lookupYN ],
+            // - File repository
+            [ $GLOBALS['lang']['app_04'], 'file_repository', $lookupYN ],
+            // - Rando
+            [ $GLOBALS['lang']['app_21'], [ 'random_setup', 'random_dashboard', 'random_perform' ],
+              [ $GLOBALS['lang']['rights_142'], $GLOBALS['lang']['rights_143'],
+                $GLOBALS['lang']['rights_144'] ] ],
+            // - Data quality
+            [ $GLOBALS['lang']['app_20'], [ 'data_quality_design', 'data_quality_execute' ],
+              [ $GLOBALS['lang']['design_248'] . '/' . $GLOBALS['lang']['global_27'],
+                $GLOBALS['lang']['dataqueries_80'] ] ],
+            // - Data resolution workflow
+            [ $GLOBALS['lang']['dataqueries_137'], 'data_quality_resolution', $lookupDRW ],
+            // - API
+            [ $GLOBALS['lang']['setup_77'], [ 'api_export', 'api_import' ],
+              [ $GLOBALS['lang']['global_71'], $GLOBALS['lang']['global_72'] ] ],
+            // - Mobile app
+            [ $GLOBALS['lang']['global_118'], [ 'mobile_app', 'mobile_app_download_data' ],
+              [ $GLOBALS['lang']['design_100'], $GLOBALS['lang']['mobile_app_27'] ] ],
+            // - Record locking customization
+            [ $GLOBALS['lang']['app_11'], 'lock_record_customize', $lookupYN ],
+            // External Modules header
+            [ $GLOBALS['lang']['global_142'], ! empty( $listModules ) ]
+          ],
+          $listModuleData,
+          [
+            // Data Entry header
+            [ $GLOBALS['lang']['bottom_20'], true ],
+            // - Create records
+            [ $GLOBALS['lang']['rights_99'], 'record_create', $lookupYN ],
+            // - Rename records
+            [ $GLOBALS['lang']['rights_100'], 'record_rename', $lookupYN ],
+            // - Delete records
+            [ $GLOBALS['lang']['rights_101'], 'record_delete', $lookupYN ],
+            // - Lock/unlock
+            [ $GLOBALS['lang']['rights_115'], [ 'lock_record', 'lock_record_multiform' ],
+              [ $lookupLock, $GLOBALS['lang']['global_49'] ] ]
+          ] ) as $infoData )
+{
+	if ( is_bool( $infoData[1] ) )
 	{
-?>
-  <td><?php echo $lookupYN[ in_array( $moduleID,
-                                      $infoRole['external_module_config'] ) ? '1' : '0' ]; ?></td>
-<?php
+		if ( $infoData[1] )
+		{
+			echo " <tr>\n  <th>", $infoData[0], "</th>\n";
+			foreach ( $listRoles as $infoRole )
+			{
+				echo '  <th>', $module->escapeHTML( $infoRole['role_name'] ), "</th>\n";
+			}
+			echo " </tr>\n";
+		}
 	}
-?>
- </tr>
-<?php
+	elseif ( is_array( $infoData[1] ) )
+	{
+		echo " <tr>\n  <td>", $infoData[0], "</td>\n";
+		foreach ( $listRoles as $infoRole )
+		{
+			echo '  <td>';
+			$list = [];
+			foreach ( array_combine( $infoData[1], $infoData[2] ) as $key => $label )
+			{
+				if ( is_array( $label ) && $label[ $infoRole[ $key ] ] != '' )
+				{
+					$list[] = $label[ $infoRole[ $key ] ];
+				}
+				elseif ( ! is_array( $label ) && $infoRole[ $key ] == '1' )
+				{
+					$list[] = $label;
+				}
+			}
+			echo empty( $list ) ? $lookupYN['0'] : implode( ', ', $list );
+			echo "</td>\n";
+		}
+		echo " </tr>\n";
+	}
+	elseif ( is_bool( $infoData[2] ) )
+	{
+		echo " <tr>\n  <td>", $infoData[0], "</td>\n";
+		foreach ( $listRoles as $infoRole )
+		{
+			echo '  <td>',
+			     $lookupYN[ in_array( $infoData[1],
+			                          $infoRole['external_module_config'] ) ? '1' : '0' ],
+			     "</td>\n";
+		}
+		echo " </tr>\n";
+	}
+	else
+	{
+		echo " <tr>\n  <td>", $infoData[0], "</td>\n";
+		foreach ( $listRoles as $infoRole )
+		{
+			echo '  <td>', $infoData[2][ $infoRole[ $infoData[1] ] ], "</td>\n";
+		}
+		echo " </tr>\n";
+	}
 }
 ?>
  <tr>
@@ -444,9 +296,7 @@ foreach ( $listForms as $formUniqueName => $formName )
 		{
 			$list[] = $GLOBALS['lang']['global_71'] . ' ' . $lookupExport['1'];
 		}
-		echo '<td>' .
-		     htmlspecialchars( empty( $list )
-		                       ? $GLOBALS['lang']['rights_47'] : implode( ', ', $list ) ) . '</td>';
+		echo '<td>' . htmlspecialchars( empty( $list ) ? '' : implode( ', ', $list ) ) . '</td>';
 	}
 ?>
  </tr>
@@ -455,6 +305,7 @@ foreach ( $listForms as $formUniqueName => $formName )
 ?>
 </table>
 <?php
+}
 $module->ffFormattingFix( '.simpRolesTable' );
 
 // Display the project footer
